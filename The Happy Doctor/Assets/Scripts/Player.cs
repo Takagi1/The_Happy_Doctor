9e5ca1f15c58fa68806fe7b90ef452a4e63f8899
Takagi1 <44708273+Takagi1@users.noreply.github.com>
2020-01-31 21:17:11 -0500

    Movement

--- The Happy Doctor/Assets/Scripts/Player.cs
+++ The Happy Doctor/Assets/Scripts/Player.cs
@@ -1,23 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class Player : MonoBehaviour
 {
 
+    public float speed;
+
     bool bagFull = false;
+    bool facingRight = true;
     Limb.LimbType content;
+    Rigidbody2D body;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float input = Input.GetAxis("Horizontal");
+        if (input != 0)
+        {
+            transform.position += new Vector3(input * speed, 0, 0);
+        }
+
+        //Flipping Sprite
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            facingRight = true;
+        }
+
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            facingRight = false;
+        }
     }
 
     void GrabLimb(Victim victim, Limb.LimbType type)
