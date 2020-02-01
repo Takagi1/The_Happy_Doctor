20daa5a763657b1768204f536d8b4a446742619d
Takagi1 <44708273+Takagi1@users.noreply.github.com>
2020-01-31 19:19:17 -0500

    init

---
+++ The Happy Doctor/Assets/Scripts/Limb.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InjuryClass
+{
+    FINE = 0,
+    INJURED,
+    LOST
+}
+
+public enum LimbType
+{
+    LEFTARM = 0,
+    LEFTLEG,
+    RIGHTARM,
+    RIGHTLEG
+}
+
+public class Limb : MonoBehaviour
+{
+    public InjuryClass state;
+    public LimbType type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
