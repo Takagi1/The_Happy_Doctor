bfd1ffc4e7f2d56c61189a4ade7f66a94cca9b72
Takagi1 <44708273+Takagi1@users.noreply.github.com>
2020-01-31 19:51:13 -0500

    Limb fix

--- The Happy Doctor/Assets/Scripts/Limb.cs
+++ The Happy Doctor/Assets/Scripts/Limb.cs
@@ -2,23 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum InjuryClass
-{
-    FINE = 0,
-    INJURED,
-    LOST
-}
-
-public enum LimbType
-{
-    LEFTARM = 0,
-    LEFTLEG,
-    RIGHTARM,
-    RIGHTLEG
-}
-
 public class Limb : MonoBehaviour
 {
+    public enum LimbType{
+        LEFTARM,
+        LEFTLEG,
+        RIGHTARM,
+        RIGHTLEG
+    }
+
+    public enum InjuryClass
+    {
+        FINE = 0,
+        INJURED,
+        LOST
+    }
+
     public InjuryClass state;
     public LimbType type;
 
---
+++ The Happy Doctor/Assets/Scripts/Victim.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Victim : MonoBehaviour
+{
+
+    [SerializeField]
+    private float health;
+
+    public Limb leftArm;
+    public Limb leftLeg;
+    public Limb rightArm;
+    public Limb rightLeg;
+
+    
+    private void Awake()
+    {
+        //Set health to max
+        health = 1000;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftArm.state == Limb.InjuryClass.INJURED){ health -= 1; }
+        else if (leftArm.state == Limb.InjuryClass.LOST) { health -= 3; }
+
+        if (leftLeg.state == Limb.InjuryClass.INJURED) { health -= 1; }
+        else if (leftLeg.state == Limb.InjuryClass.LOST) { health -= 3; }
+
+        if (rightArm.state == Limb.InjuryClass.INJURED) { health -= 1; }
+        else if (rightArm.state == Limb.InjuryClass.LOST) { health -= 3; }
+
+        if (rightLeg.state == Limb.InjuryClass.INJURED) { health -= 1; }
+        else if (rightLeg.state == Limb.InjuryClass.LOST) { health -= 3; }
+
+        if(health <= 0)
+        {
+            //TODO: Kill victim here
+        }
+    }
+}
